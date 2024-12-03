c4135f0ac396bbc036e4f464bdec68d41bce7e19
Alix Meneses <aameneses21@gmail.com>
2024-12-03 16:42:45 -0500

    Base frontend

---
+++ Frontend/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './view/home/home.component';
+import { InformationComponent } from './view/information/information.component';
+
+export const routes: Routes = [
+    {
+        path:"",
+        redirectTo:"home",
+        pathMatch:"full"
+    },
+    {
+        path:"home",
+        component:HomeComponent
+    },
+    {
+        path:"information",
+        component:InformationComponent
+    }
+
+];
---
+++ Frontend/src/app/view/information/information.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'information',
+  standalone: true,
+  imports: [],
+  templateUrl: './information.component.html',
+  styleUrl: './information.component.scss'
+})
+export class InformationComponent {
+
+}
